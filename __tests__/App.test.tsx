9cf51c9e8b530610e5f922c63da9b6c27daf5cd9
SanketHebbal <sankethebbal100@gmail.com>
2025-11-24 17:05:34 +0530

    removed unused imports

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,10 +2,6 @@
  * @format
  */
 
-import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
-
 test('2 + 2', () => {
   expect(2 + 2).toBe(4);
 });
